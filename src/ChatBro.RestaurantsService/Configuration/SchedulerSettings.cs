29ebd6d38f124e51180bc3682aea6d062b50beec
Ilya <siavol@gmail.com>
2025-10-15 19:26:31 +0300

    Update WarmupJobCron schedule to run at 6 AM on weekdays

--- src/ChatBro.RestaurantsService/Configuration/SchedulerSettings.cs
+++ src/ChatBro.RestaurantsService/Configuration/SchedulerSettings.cs
@@ -2,5 +2,5 @@ namespace ChatBro.RestaurantsService.Configuration;
 
 public class SchedulerSettings
 {
-    public string WarmupJobCron { get; init; } = "0 9 * * 1-5";
+    public string WarmupJobCron { get; init; } = "0 6 * * 1-5";
 }
